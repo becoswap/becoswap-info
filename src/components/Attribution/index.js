9b1bdf0a9fc30ca6cb618efc7509478bf42120e1
becoswap-dev <cool@becoswap.com>
2021-05-27 21:49:03 +0700

    first commit

---
+++ src/components/Attribution/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="https://github.com/becoswap/becoswap-info" rel="noopener noreferrer" target="_blank">
+      Github
+    </a>{' '}
+    |{' '}
+    <a href="https://becoswap.com" rel="noopener noreferrer" target="_blank">
+      Becoswap
+    </a>{' '}
+    |{' '}
+    <a href="#" rel="noopener noreferrer" target="_blank">
+      GIF
+    </a>
+  </p>
+)
+
+export default Attribution
